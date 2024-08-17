8ea8edb7a835ff01ed87285b267377d2a9b4ce85
manasa1826 <ponnadamanasa06@gmail.com>
2024-08-17 17:00:54 +0530

    manasa

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "fun",
+    version = "0.0.0",
+    author = "manasa1826",
+    author_email = "ponnadamanasa06@gmail.com",
+    packages = find_packages()
+)
